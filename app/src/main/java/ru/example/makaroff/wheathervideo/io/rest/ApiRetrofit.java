4bbab57f5ae4e3be570768d6c79b11743a7e5c00
Oleg Makarov <makaroff-297991@mail.ru>
2016-06-08 12:46:38 +0700

    start weather

--- app/src/main/java/ru/example/makaroff/wheathervideo/io/rest/ApiRetrofit.java
+++ app/src/main/java/ru/example/makaroff/wheathervideo/io/rest/ApiRetrofit.java
@@ -1,7 +1,4 @@
 package ru.example.makaroff.wheathervideo.io.rest;
 
-/**
- * Created by 12390 on 08.06.2016.
- */
 public class ApiRetrofit {
 }
--- app/src/main/java/ru/example/makaroff/wheathervideo/ui/WeatherFragment.java
+++ app/src/main/java/ru/example/makaroff/wheathervideo/ui/WeatherFragment.java
@@ -1,11 +1,13 @@
 package ru.example.makaroff.wheathervideo.ui;
 
+import android.support.v4.app.Fragment;
+
 import org.androidannotations.annotations.EFragment;
 
 import ru.example.makaroff.wheathervideo.R;
 
 @EFragment(R.layout.fragment_weather)
-public class WeatherFragment {
+public class WeatherFragment extends Fragment {
 
 
 }
